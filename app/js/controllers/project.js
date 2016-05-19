121a667674ba05a2d1db08ab6d2703faeba71d46
kallexander <kalle.bornemark@gmail.com>
2016-05-19 16:43:13 +0200

    Made buttons visible depending on project status and user role

--- app/js/controllers/project.js
+++ app/js/controllers/project.js
@@ -12,6 +12,7 @@ function ProjectCtrl(ProjectService, CommentService, StorageService, AppSettings
   vm.user = window.user;
   vm.token = storage.get('api_token');
   vm.comment = {};
+  vm.status = project.status;
 
   vm.postComment = () => {
     CommentService.store('projects', vm.project.id, vm.comment.body)
@@ -25,39 +26,46 @@ function ProjectCtrl(ProjectService, CommentService, StorageService, AppSettings
     return (config.PROJECT_STATUSES.PUBLISHED != project.status || vm.user.role_id == config.ROLES.ADMINISTRATOR);
   };
 
-  vm.userIsStudent = () => {
-    return false;
+  vm.userIsTeacher = () => {
+    return vm.user.role_id == config.PROJECT_ROLES.TEACHER;
+  };
+
+  vm.userIs = (role) => {
+    console.log("userIs(" + role + "): " + (vm.user.role_id == role));
+    return vm.user.role_id == role;
   };
 
   vm.userCanPublish = () => {
-    let userIsStudent;
-    let projectUser;
-    let canPublish;
-    if (project.participants) {
-      projectUser = $filter('filter')(project.participants, { id : vm.user.id })[0];
-      if (projectUser) {
-        canPublish = projectUser.pivot.project_role_id == config.PROJECT_ROLES.STUDENT
-              && (project.status == config.PROJECT_STATUSES.COMPLETED || project.status == config.PROJECT_STATUSES.PUBLISHED);
+    if (vm.project.status == config.PROJECT_STATUSES.COMPLETED || vm.project.status == config.PROJECT_STATUSES.PUBLISHED) {
+      if (vm.userIs(config.ROLES.ADMINISTRATOR)) {
+        console.log("USER---CAN---PUBLISH: " + true);
+        return true;
+      } else if (project.participants) {
+          let projectUser = $filter('filter')(vm.project.participants, { id : vm.user.id })[0];
+          if (projectUser) {
+            console.log("USER---CAN---PUBLISH: " + true);
+            return projectUser.pivot.project_role_id == config.PROJECT_ROLES.STUDENT;
+          }
+        }
       }
-    }
-    console.log("canPublish: " + (canPublish));
-    return canPublish;
+    console.log("USER---CAN---PUBLISH: " + false);
+    return false;
   }
 
   vm.userCanEditProject = () => {
-    let isAdmin = (vm.user.role_id == config.ROLES.ADMINISTRATOR);
-    let isTeacher;
-    let isSupervisor;
-
-    if (!isAdmin) {
-      let projectUser = $filter('filter')(project.participants, { id : vm.user.id })[0];
-      if (projectUser) {
-        isTeacher = projectUser.pivot.project_role_id == config.PROJECT_ROLES.TEACHER;
-        isSupervisor = projectUser.pivot.project_role_id == config.PROJECT_ROLES.SUPERVISOR;
+    if (vm.project.status != config.PROJECT_STATUSES.PUBLISHED) {
+      if (vm.userIs(config.ROLES.ADMINISTRATOR)) {
+        return true;
+      } else {
+        let projectUser = $filter('filter')(project.participants, { id : vm.user.id })[0];
+        if (projectUser) {
+          let isTeacher = projectUser.pivot.project_role_id == config.PROJECT_ROLES.TEACHER;
+          let isSupervisor = projectUser.pivot.project_role_id == config.PROJECT_ROLES.SUPERVISOR;
+          return (isTeacher || isSupervisor);
+        }
       }
     }
-
-    return (isAdmin || isTeacher || isSupervisor);
+    return false;
   };
 
   vm.userCanManageProject = () => {
@@ -72,21 +80,14 @@ function ProjectCtrl(ProjectService, CommentService, StorageService, AppSettings
     let statuses = {};
 
     for (var key in config.PROJECT_STATUS_IDS) {
-      if (config.PROJECT_STATUS_IDS.hasOwnProperty(key) && key != vm.project.status) {
+      let isTeacher = vm.userIsTeacher();
+      if (config.PROJECT_STATUS_IDS.hasOwnProperty(key)
+        && key != vm.project.status
+        && !(key == config.PROJECT_STATUSES.PUBLISHED)) {
         statuses[key] = config.PROJECT_STATUS_IDS[key];
       }
     }
     return statuses;
-  };
-
-  vm.updateStatus = () => {
-    var payload = {
-      status: vm.status
-    };
-    ProjectService.update(vm.project.id, payload)
-      .then(project => {
-        vm.project = project;
-      })
   };
 
   vm.isPublished = () => {
@@ -94,8 +95,22 @@ function ProjectCtrl(ProjectService, CommentService, StorageService, AppSettings
     return vm.project.status == vm.config.PROJECT_STATUSES.PUBLISHED;
   };
 
+  vm.isCompleted = () => {
+    return vm.project.status == vm.config.PROJECT_STATUSES.COMPLETED;
+  };
+
   vm.publish = (shouldPublish) => {
     ProjectService.publish(vm.project.id, shouldPublish)
+      .then(project => {
+        vm.project = project;
+      })
+  };
+
+  vm.updateStatus = (setStatus) => {
+    var payload = {
+      status: setStatus
+    };
+    ProjectService.update(vm.project.id, payload)
       .then(project => {
         vm.project = project;
       })
