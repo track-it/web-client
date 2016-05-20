1717581df448fa0b211cd3d205bfb2b3f5957749
nekosaur <albert@kaaman.nu>
2016-05-20 12:15:18 -0400

    Hopefully fixed bug where user was logged out when refreshing page

--- app/js/controllers/adfsLogin.js
+++ app/js/controllers/adfsLogin.js
@@ -1,7 +1,11 @@
-function AdfsLoginCtrl($state, me) {
+function AdfsLoginCtrl(AppSettings, StorageService, $state, me) {
   'ngInject';
 
+  const config = AppSettings;
+
   if (me) {
+  	StorageService.put(config.API_TOKEN, me.api_token);
+
     $state.transitionTo('account');
   } else {
     $state.transitionTo('home');
